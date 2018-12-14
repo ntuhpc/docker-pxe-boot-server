271992b857b1c154a49453a466bcbf32dbf2b1c1
wudaown <wudaown@gmail.com>
2018-12-14 22:09:07 +0800

    - fixed bug in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
 set -u 
 if [ "$#" -ne 4 ]; then
-    echo 'Usage: prepare.sh 10.0.0.238 10.0.0.0 10.0.0.10 10.0.0.20'
+    echo 'Usage: prepare.sh IPADDR SUBNET STARTRANGE ENDRANGE'
     exit 1
 fi
 
 cp ./conf/* .
 
-10.0.0.238=$1
-10.0.0.0=$2
-10.0.0.10=$3
-10.0.0.20=$4
+IPADDR=$1
+SUBNET=$2
+STARTRANGE=$3
+ENDRANGE=$4
 
-find ./ -type f -exec sed -i -e "s/10.0.0.238/$10.0.0.238/g" {} \;
-find ./ -type f -exec sed -i -e "s/10.0.0.0/$10.0.0.0/g" {} \;
-find ./ -type f -exec sed -i -e "s/10.0.0.10/$10.0.0.10/g" {} \;
-find ./ -type f -exec sed -i -e "s/10.0.0.20/$10.0.0.20/g" {} \;
-# sed -e "s/10.0.0.238/$1/g" ./conf/default > ./default
-# sed -e "s/10.0.0.238/$1/g" ./conf/ks-auto.cfg > ./ks-auto.cfg
-
+find . -maxdepth 1 ! -name 'prepare.sh' -maxdepth 1 -type f -exec sed -i  "s/IPADDR/$IPADDR/g" {} \;
+find . -maxdepth 1 ! -name 'prepare.sh' -maxdepth 1 -type f -exec sed -i  "s/SUBNET/$SUBNET/g" {} \;
+find . -maxdepth 1 ! -name 'prepare.sh' -maxdepth 1 -type f -exec sed -i  "s/STARTRANGE/$STARTRANGE/g" {} \;
+find . -maxdepth 1 ! -name 'prepare.sh' -maxdepth 1 -type f -exec sed -i  "s/ENDRANGE/$ENDRANGE/g" {} \;
